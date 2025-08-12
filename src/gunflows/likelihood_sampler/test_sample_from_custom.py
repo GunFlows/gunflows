e666077861acc001eb0f6172be516a0054bdbde4
lgiannes <lorenzo.giannessi@unige.ch>
2025-08-12 11:04:17 +0200

    update plots live (every 100 throws

--- src/gunflows/likelihood_sampler/test_sample_from_custom.py
+++ src/gunflows/likelihood_sampler/test_sample_from_custom.py
@@ -10,6 +10,71 @@ from gunflows.likelihood_sampler import LikelihoodSampler
 from gunflows.likelihood_sampler import pygundam_utils
 
 
+def draw_logp_logq(log_p, log_q, bestfit_nll, out_dir):
+
+    # shift for visualization
+    A = np.mean(log_p - bestfit_nll)
+    B = np.mean(log_q)
+    log_p = log_p + (B-A)
+
+    plt.figure(figsize=(8, 6))
+    plt.hist(log_p[log_p<5000] , alpha=0.7, density=True,
+             color='lightblue', bins=100, label='-log p - bestfit_nll')
+    plt.xlabel('NLL')
+    plt.ylabel('Density')
+    plt.title('NLL Distribution')
+    plt.savefig(out_dir+'/NLL_distribution.png', dpi=100, bbox_inches='tight')
+    plt.close()
+
+    plt.hist(log_q, alpha=0.7, density=True,
+             color='orange', bins=100,  label='-log q')
+    # gaussian fit
+    mu, std = np.mean(log_q), np.std(log_q)
+    if std == 0:
+        std = 1e-6  # avoid division by zero in case of constant values
+    # write mu and std on the canvas
+    # x = np.linspace(mu - 4*std, mu + 4*std, 1000)
+    # p = (1 / (std * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / std) ** 2)
+    # plt.plot(x, p, color='red', linewidth=2, label='Gaussian Fit')
+    # plt.plot([], [], ' ', label='$\mu$ = {:.2f}, $\sigma$ = {:.2f}'.format(mu, std))
+    plt.legend()
+    plt.xlabel('NLL')
+    plt.ylabel('Density')
+    plt.title('gNLL Distribution')
+    plt.savefig(out_dir+'/gNLL_distribution.png', dpi=100, bbox_inches='tight')
+
+    plt.figure(figsize=(8, 6))
+    # 2d histogram of NLL vs gNLL
+    h = plt.hist2d(log_p - bestfit_nll, log_q, bins=100, cmap='viridis', density=True, norm='log')
+    plt.colorbar(h[3], label='Log Density')
+    # diagonal line
+    plt.plot([min(log_p - bestfit_nll), max(log_p - bestfit_nll)], [min(log_p - bestfit_nll), max(log_p - bestfit_nll)], color='red', linestyle='--')
+    plt.xlabel('NLL')
+    plt.ylabel('gNLL')
+    # save pics
+    plt.savefig(out_dir+'/NLL_gNLL_histogram.png', dpi=100, bbox_inches='tight')
+    plt.close()
+    # NLL and gNLL overlaid
+    plt.figure(figsize=(8, 6))
+    plt.hist(log_q, alpha=0.7, density=True,
+             color='lightblue', bins=100,  label='-log q')
+    plt.hist(log_p - bestfit_nll, alpha=0.7, density=True,
+             color='orange', bins=100, label='-log p - bestfit_nll')
+    plt.xlabel('NLL')
+    plt.ylabel('Density')
+    plt.title('NLL and gNLL Distribution')
+    # overlay line at best fit NLL
+    plt.axvline(bestfit_nll, color='red', linestyle='--', label='Best Fit NLL')
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+    # save the plot
+    plt.legend()
+    plt.savefig(out_dir+'/NLL_gNLL_overlay.png', dpi=100, bbox_inches='tight')
+    plt.close()
+
+
+
+
 def log_multivariate_normal_pdf(x, mean, cov):
     eigen_parameter_values = convert_to_eigenspace(random_parameter_values, mean=bestfit_parameter_values, cov=postfit_covariance)
     log_prob = len(eigen_parameter_values)*0.5*np.log(2*np.pi) + sum(eigen_parameter_values**2)
@@ -68,6 +133,9 @@ parameter_names = likelihood_sampler.get_parameter_names()
 
 start_time = time.time()
 n = int(args.n)
+N_n = 100  # update plots every N_n samples
+out_dir = 'img'
+os.makedirs(out_dir, exist_ok=True)
 # sample parameters from a custom distribution
 NLL_list = []
 params_list = []
@@ -117,8 +185,25 @@ for i in range(n):
     logq_list.append(log_q)
     NLL_list.append(NLL)
 
+    # update plots every N_n samples
+    if (i+1) % N_n == 0:
+        params_dict = likelihood_sampler.generate_dataset_dictionary(params_list, logq_list, NLL_list)
+        output_file = args.o
+        np.savez(output_file, **params_dict)
+        print(f"Saved dataset to {output_file}")
+        data = params_dict
+        bestfit_nll = params_dict['bestfit_nll']
+        parameter_names = data['par_names']
+        params_array = np.array(data['data'])
+        log_p = np.array(data['log_p'])
+        log_q = np.array(data['log_q'])
+        # draw NLL and gNLL
+        draw_logp_logq(log_p, log_q, bestfit_nll, out_dir)
 
-print(f"bestfit_parameter_values: {pygundam_utils.big_vector_summary(bestfit_parameter_values)}")
+
+
+
+# print(f"bestfit_parameter_values: {pygundam_utils.big_vector_summary(bestfit_parameter_values)}")
 
 
 end_time = time.time()
@@ -127,23 +212,14 @@ print(f"Time for 1 LH evaluation: {duration/n*1000} ms")
 
 # get the dictionary and save it
 params_dict = likelihood_sampler.generate_dataset_dictionary(params_list, logq_list, NLL_list)
-output_file = "test.npz"
-# save the dictionary to npz file
-np.savez(output_file, **params_dict)
 print(f"Saved dataset to {output_file}")
 
 # plots
 
 # NLL and gNLL
 # draw all the parameter distributions, overlaying the prior and postfit values
-out_dir = 'img'
-os.makedirs(out_dir, exist_ok=True)
-data = params_dict
-bestfit_nll = params_dict['bestfit_nll']
-parameter_names = data['par_names']
-params_array = np.array(data['data'])
-log_p = np.array(data['log_p'])
-log_q = np.array(data['log_q'])
+
+
 
 # plot eigen parameters
 # for i,param in enumerate(eigen_params_list):
@@ -161,67 +237,9 @@ log_q = np.array(data['log_q'])
 
 
 
-A = np.mean(log_p - bestfit_nll)
-B = np.mean(log_q)
-print(f"Mean of -(log p - bestfit_nll): {A}, Mean of -log q: {B}")
 
-log_p = log_p + (B-A)
 
-# draw NLL and gNLL
-plt.figure(figsize=(8, 6))
-plt.hist(log_p[log_p<5000] , alpha=0.7, density=True,
-         color='lightblue', bins=100, label='-log p - bestfit_nll')
-plt.xlabel('NLL')
-plt.ylabel('Density')
-plt.title('NLL Distribution')
-plt.savefig(out_dir+'/NLL_distribution.png', dpi=100, bbox_inches='tight')
-plt.close()
 
-plt.hist(log_q, alpha=0.7, density=True,
-         color='orange', bins=100,  label='-log q')
-# gaussian fit
-mu, std = np.mean(log_q), np.std(log_q)
-if std == 0:
-    std = 1e-6  # avoid division by zero in case of constant values
-# write mu and std on the canvas
-x = np.linspace(mu - 4*std, mu + 4*std, 1000)
-p = (1 / (std * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / std) ** 2)
-plt.plot(x, p, color='red', linewidth=2, label='Gaussian Fit')
-plt.plot([], [], ' ', label='$\mu$ = {:.2f}, $\sigma$ = {:.2f}'.format(mu, std))
-plt.legend()
-plt.xlabel('NLL')
-plt.ylabel('Density')
-plt.title('gNLL Distribution')
-plt.savefig(out_dir+'/gNLL_distribution.png', dpi=100, bbox_inches='tight')
-
-plt.figure(figsize=(8, 6))
-# 2d histogram of NLL vs gNLL
-h = plt.hist2d(log_p - bestfit_nll, log_q, bins=100, cmap='viridis', density=True, norm='log')
-plt.colorbar(h[3], label='Log Density')
-# diagonal line
-plt.plot([min(log_p - bestfit_nll), max(log_p - bestfit_nll)], [min(log_p - bestfit_nll), max(log_p - bestfit_nll)], color='red', linestyle='--')
-plt.xlabel('NLL')
-plt.ylabel('gNLL')
-# save pics
-plt.savefig(out_dir+'/NLL_gNLL_histogram.png', dpi=100, bbox_inches='tight')
-plt.close()
-# NLL and gNLL overlaid
-plt.figure(figsize=(8, 6))
-plt.hist(log_q, alpha=0.7, density=True,
-         color='lightblue', bins=100,  label='-log q')
-plt.hist(log_p - bestfit_nll, alpha=0.7, density=True,
-         color='orange', bins=100, label='-log p - bestfit_nll')
-plt.xlabel('NLL')
-plt.ylabel('Density')
-plt.title('NLL and gNLL Distribution')
-# overlay line at best fit NLL
-plt.axvline(bestfit_nll, color='red', linestyle='--', label='Best Fit NLL')
-plt.grid(True, alpha=0.3)
-plt.tight_layout()
-# save the plot
-plt.legend()
-plt.savefig(out_dir+'/NLL_gNLL_overlay.png', dpi=100, bbox_inches='tight')
-plt.close()
 
 # plot the grid of parameters to show correlations
 start_dim = 652
@@ -231,14 +249,14 @@ for i in range(start_dim,start_dim+ndim):
     for j in range(start_dim,start_dim+ndim):
         a = ax[i-start_dim, j-start_dim]
         if i == j:
-            a.hist(params_array[:, i], bins=50, density=True, color='lightblue', edgecolor='black')
+            a.hist(params_array[:, i], bins=50, density=True, color='viridis', edgecolor='black')
             a.axvline(bestfit_parameter_values[i], color='red', linestyle='--', label='Best Fit')
             a.axvline(prior_parameter_values[i], color='green', linestyle='--', label='Prior')
             a.set_xlabel(parameter_names[i])
             a.set_ylabel('Density')
             a.legend()
         else:
-            a.scatter(params_array[:, i], params_array[:, j], alpha=0.1, color='lightblue')
+            a.scatter(params_array[:, i], params_array[:, j], alpha=0.1, color='viridis')
             a.axvline(bestfit_parameter_values[i], color='red', linestyle='--', label='Best Fit')
             a.axhline(bestfit_parameter_values[j], color='red', linestyle='--')
             a.set_xlabel(parameter_names[i])
