3f57f9725a3037014c561fcd085ac6ff7f7051ff
lgiannes <lorenzo.giannessi@unige.ch>
2025-08-13 14:35:40 +0200

    test files
    sovled conflicts between abobab and local

--- tests/job_sample_from_covariance_asimov_run5.sh
+++ tests/job_sample_from_covariance_asimov_run5.sh
@@ -1,22 +1,22 @@
 #!/bin/env bash
-#SBATCH --job-name=sample_cov_asimov
-#SBATCH --partition=shared-cpu
+#SBATCH --job-name=sample_cov_asimov_run5
+#SBATCH --partition=private-dpnc-cpu,shared-cpu
 #SBATCH --time=8:00:00
 #SBATCH --cpus-per-task=12
-#SBATCH --output=sample_from_cov_asimov_%A_%a.out
-#SBATCH --error=sample_from_cov_asimov_%A_%a.err
-#SBATCH --mem=30000 # in MB
+#SBATCH --output=sample_from_cov_asimov_run5_%A_%a.out
+#SBATCH --error=sample_from_cov_asimov_run5_%A_%a.err
+#SBATCH --mem-per-cpu=8000 # in MB
 #SBATCH --mail-type=ALL
 
 index=${SLURM_ARRAY_TASK_ID}
 time=$(date +%s%N)
 seed=$(($index + $time))
 CONFIG_FILE="output/Fitter/gundamFitter_configOa2021_With_allowEigenDecompWithBounds_Asimov.root"
-OUTPUT_FILE="oa2022_asimov_${seed}.npz"
-N=100000
+OUTPUT_FILE="oa2022_asimovRun5/batch${index}.npz"
+N=50000
 
 
-SCRIPTARGS="-m gunflows.likelihood_sampler.test_sample_from_custom -o ${OUTPUT_FILE} -c ${CONFIG_FILE} -n ${N} -a -t 12 " #-s ${seed}
+SCRIPTARGS="-m gunflows.likelihood_sampler.test_sample_from_custom -o ${OUTPUT_FILE} -c ${CONFIG_FILE} -of override/onlyRun5.yaml -n ${N} -a -t 12 " #-s ${seed}
 
 APPTAINER_OPTIONS="--nv --cleanenv \
   --env PYTHONNOUSERSITE=1 \
@@ -27,7 +27,6 @@ APPTAINER_OPTIONS="--nv --cleanenv \
   --pwd /workspace/config/GundamInputOA2021"
 
 IMAGE_PATH="/home/shares/sanchezf/gundam_n_flow/GuNFlows_dev/env/containers/ml_image2.sif"
-
 
 echo "Starting job: " `date`
 
