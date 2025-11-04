8eda38dac5c05427996aa128b9de960ddcb2cfa4
lgiannes <lorenzo.giannessi@unige.ch>
2025-11-04 13:36:10 +0100

    instructions to "sample_and_compare.sh"

--- bash/sample_and_compare.sh
+++ bash/sample_and_compare.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1,VramPerGpu:24G
 #SBATCH --output=logs/sample_and_compare_%j.out
 #SBATCH --error=logs/sample_and_compare_%j.err
